d8a6b1499df8c8b1f19da483adabb98bffafdd80
MFomins <35532347+MFomins@users.noreply.github.com>
2020-01-21 19:53:42 +0200

    Update LD2_1.java

--- LD2_1.java
+++ LD2_1.java
@@ -1,12 +1,18 @@
 //171rdb337      %2    = uzd 1.
 //  Recursive Digit Sum
 
-package Recursion;
+package superdigit;
 
-public class recursion {
-  	public static long recursion(long x){
-  
-  
-  
-  
+public class SuperDigit {
+	
+	public static int superDigit(int n){
+		String num = new Integer(n).toString();
+		int numSum = num.chars().map(Character::getNumericValue).sum();
+		int numCount = num.length();
+		if (numCount == 1) 
+			return numSum;
+		else
+			return superDigit(numSum);
+	}	
+
 }
