2bd447c4793ee676e747db83e8030ac4889be799
MFomins <35532347+MFomins@users.noreply.github.com>
2020-01-23 11:07:25 +0200

    4 lab darbs, conjure

---
+++ core.clj
@@ -0,0 +1,18 @@
+(ns clofp-1.core)
+
+(defn clojurefp [vars]
+
+    (float 
+    (/  
+      ( reduce + 
+               ( map (fn[x] (val x))
+                     ( filter (fn[k] (.endsWith (key k) "a")) vars )
+               )
+      )
+      
+      ( count
+        ( filter (fn[k] (.endsWith (key k) "a")) vars)
+      )
+    ) 
+  )
+)
